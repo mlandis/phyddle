a0dd3f0b2047de091e55a87cf8e0d95efee24a91
Michael Landis <mlandis@gmail.com>
2023-05-07 20:34:45 -0500

    update notes

--- code/run_format.py
+++ code/run_format.py
@@ -12,7 +12,7 @@ import Utilities
 # LOAD PIPELINE CONFIG #
 ########################
 
-my_config = Utilities.load_config('phyddle_config', arg_overwrite=True)
+my_config = Utilities.load_config('config', arg_overwrite=True)
 
 my_all_args = my_config.my_all_args
 my_fmt_args = my_config.my_fmt_args
