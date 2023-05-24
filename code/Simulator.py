d605c4dff27c482d0319e5484ee09ce4944bc6ca
Michael Landis <mlandis@gmail.com>
2023-05-24 14:45:44 -0500

    do not clean sci not

--- code/Simulator.py
+++ code/Simulator.py
@@ -148,8 +148,8 @@ class Simulator:
 
         # record labels (simulating parameters)
         param1_str,param2_str = Utilities.param_dict_to_str(self.model.params)
-        param1_str = Utilities.clean_scientific_notation(param1_str)
-        param2_str = Utilities.clean_scientific_notation(param2_str)
+        #param1_str = Utilities.clean_scientific_notation(param1_str)
+        #param2_str = Utilities.clean_scientific_notation(param2_str)
         Utilities.write_to_file(param1_str, param1_fn)
         Utilities.write_to_file(param2_str, param2_fn)
 
@@ -157,7 +157,7 @@ class Simulator:
         cblvs_str = np.array2string(cblvs, separator=',', max_line_width=1e200, threshold=1e200, edgeitems=1e200, precision=10, floatmode='maxprec')
         cblvs_str = cblvs_str.replace(' ','').replace('.,',',').strip('[].') + '\n'
         #cblvs_str = re.sub( '\.0+E\+0+', '', cblvs_str)
-        cblvs_str = Utilities.clean_scientific_notation(cblvs_str)
+        #cblvs_str = Utilities.clean_scientific_notation(cblvs_str)
         #print(cblvs_str)
         Utilities.write_to_file(cblvs_str, cblvs_fn)
 
@@ -172,7 +172,7 @@ class Simulator:
         # record summ stat data
         ss = Utilities.make_summ_stat(tre_fn, geo_fn, vecstr2int)
         ss_str = Utilities.make_summ_stat_str(ss)
-        ss_str = Utilities.clean_scientific_notation(ss_str) #re.sub( '\.0+E\+0+', '', ss_str)
+        #ss_str = Utilities.clean_scientific_notation(ss_str) #re.sub( '\.0+E\+0+', '', ss_str)
         Utilities.write_to_file(ss_str, ss_fn)
 
         # return status string
