486f1595442d71e808a75cdf0e87f7a3744bf1b2
Michael Landis <mlandis@gmail.com>
2023-07-09 11:42:27 -0500

    testpy for sim/fmt

---
+++ tests/config.py
@@ -0,0 +1,110 @@
+#==============================================================================#
+# Default phyddle config file                                                  #
+#==============================================================================#
+
+# external import
+import scipy as sp
+
+# helper variables
+num_char = 2
+num_states = 3
+
+args = {
+    
+    #-------------------------------#
+    # Project organization          #
+    #-------------------------------#
+    'proj'           : 'test',             # directory name for pipeline project
+    'steps'          : 'all',                         # steps to run? all, sim, fmt, lrn, prd, plt
+    'sim_dir'        : './workspace/raw_data',       # directory for simulated data
+    'fmt_dir'        : './workspace/tensor_data',    # directory for tensor-formatted data
+    'net_dir'        : './workspace/network',        # directory for trained network
+    'plt_dir'        : './workspace/plot',           # directory for plotted figures
+    'pred_dir'       : './workspace/predict',        # directory for predictions on new data
+    'pred_prefix'    : 'new.1',             # prefix for new dataset to predict
+    
+    #-------------------------------#
+    # Multiprocessing               #
+    #-------------------------------#
+    'use_parallel'   : False,               # use multiprocessing to speed up jobs?
+    'num_proc'       : -2,                  # how many CPUs to use (-2 means all but 2)
+    
+    #-------------------------------#
+    # Model Configuration           #
+    #-------------------------------#
+    'model_type'         : 'geosse',        # model type defines general states and events
+    'model_variant'      : 'equal_rates',   # model variant defines rate assignments
+    'num_char'           : num_char,        # number of evolutionary characters
+    'num_states'         : num_states,      # number of states per discrete character
+    'rv_fn'              : {                # distributions for model parameters
+        'w': sp.stats.expon.rvs,
+        'e': sp.stats.expon.rvs,
+        'd': sp.stats.expon.rvs,
+        'b': sp.stats.expon.rvs
+    },
+    'rv_arg'             : {                # loc/scale/shape for model parameter dists
+        'w': { 'scale' : 0.2 },
+        'e': { 'scale' : 0.1 },
+        'd': { 'scale' : 0.1 },
+        'b': { 'scale' : 0.5 }
+    },
+
+    #-------------------------------#
+    # Simulating Step settings      #
+    #-------------------------------#
+    'sim_method'        : 'command',        # command, master, [phylojunction], ...
+    'sim_command'       : 'Rscript sim_one.R',   # exact command string, argument is output file prefix
+    'sim_logging'       : 'verbose',        # verbose, compressed, or clean
+    'start_idx'         : 0,                # first simulation replicate index
+    'end_idx'           : 1,                # last simulation replicate index
+    'sample_population' : ['S'],            # name of population to sample
+    'stop_time'         : 10,               # time to stop simulation
+    'min_num_taxa'      : 10,               # min number of taxa for valid sim
+    'max_num_taxa'      : 500,              # max number of taxa for valid sim
+
+    #-------------------------------#
+    # Formatting Step settings      #
+    #-------------------------------#
+    'tree_type'         : 'extant',         # use model with serial or extant tree
+    'tree_width_cats'   : [ 200, 500 ],     # tree width categories for phylo-state tensors
+    'tree_encode_type'  : 'height_brlen',   # how to encode phylo brlen? height_only or height_brlen
+    'char_encode_type'  : 'one_hot',        # how to encode discrete states? one_hot or integer 
+    'param_pred'        : [                 # model parameters to predict (labels)
+        'birth', 'death', 'state_rate'
+    ],
+    'param_data'        : [],               # model parameters that are known (aux. data)
+    'tensor_format'     : 'hdf5',           # save as compressed HDF5 or raw csv
+    'save_phyenc_csv'   : True,             # save intermediate phylo-state vectors to file
+
+    #-------------------------------#
+    # Learning Step settings        #
+    #-------------------------------#
+    'learn_method'      : 'param_est',      # what is the learning task? param_est or model_test
+    'tree_width'        : 200,              # tree width category used to train network
+    'num_epochs'        : 20,               # number of training intervals (epochs)
+    'prop_test'         : 0.05,             # proportion of sims in test dataset
+    'prop_validation'   : 0.05,             # proportion of sims in validation dataset
+    'prop_calibration'  : 0.20,             # proportion of sims in CPI calibration dataset 
+    'cpi_coverage'      : 0.95,             # coverage level for CPIs
+    'batch_size'        : 128,              # number of samples in each training batch
+    'loss'              : 'mse',            # loss function for learning
+    'optimizer'         : 'adam',           # optimizer for network weight/bias parameters
+    'metrics'           : ['mae', 'acc'],   # recorded training metrics
+
+    #-------------------------------#
+    # Plotting Step settings        #
+    #-------------------------------#
+    'plot_train_color'      : 'blue',       # plot color for training data
+    'plot_test_color'       : 'purple',     # plot color for test data
+    'plot_val_color'        : 'red',        # plot color for validation data
+    'plot_aux_color'        : 'green',      # plot color for input auxiliary data
+    'plot_label_color'      : 'orange',     # plot color for labels (params)
+    'plot_pred_color'       : 'black'       # plot color for predictions
+
+    #-------------------------------#
+    # Predicting Step settings      #
+    #-------------------------------#
+    # prediction already handled by previously defined settings
+    # no prediction-specific settings currently implemented
+}
+
---
+++ tests/test_phyddle.py
@@ -0,0 +1,86 @@
+from phyddle import Utilities
+from phyddle import ModelLoader
+from phyddle import Simulating
+from phyddle import Formatting
+from phyddle import Learning
+from phyddle import Predicting
+from phyddle import Plotting
+
+# set random seed
+import numpy as np
+import h5py
+import os
+
+#-----------------------------------------------------------------------------#
+
+def inc(x):
+    return x + 1
+
+def test_inc():
+    assert inc(3) == 4
+
+#-----------------------------------------------------------------------------#
+
+def test_onehot_encoding():
+    s = Utilities.convert_nexus_to_array('data/sim.1.dat.nex', 'onehot', 333)
+    print(s)
+    assert s != ''
+
+#-----------------------------------------------------------------------------#
+
+def run_pipeline():
+
+    my_args = Utilities.load_config('config', arg_overwrite=True, args=[])
+    
+    proj = my_args['proj']
+    sim_dir = my_args['sim_dir']
+    fmt_dir = my_args['fmt_dir']
+
+    ################
+    # RUN PIPELINE #
+    ################
+
+    # Step 1: simulate training data
+    my_mdl = ModelLoader.load(my_args)
+    my_sim = Simulating.load(my_args, my_mdl)
+    my_sim.run()
+
+
+    # Step 2: format training data into tensors
+    my_fmt = Formatting.load(my_args)
+    my_fmt.run()
+
+    # Step 3: train network with training data
+    #my_lrn = Learning.load(my_args)
+    #my_lrn.run()
+
+    # Step 4: predict for new dataset
+    #my_prd = Predicting.load(my_args)
+    #pred_prefix = f"{my_args['pred_dir']}/{my_args['proj']}/{my_args['pred_prefix']}"
+    #my_fmt.encode_one(tmp_fn=pred_prefix, idx=-1, save_phyenc_csv=True)
+    #my_prd.run()
+
+    # Step 5: plot results
+    #my_plt = Plotting.load(my_args)
+    #my_plt.run()
+
+    cdvs_fn = f'{sim_dir}/{proj}/sim.0.cdvs.csv'
+    cdvs = np.loadtxt(cdvs_fn, delimiter=',')
+    cdvs_sum = round(np.sum(cdvs), ndigits=8)
+    # print(cdvs_sum)
+
+    return cdvs_sum
+
+def test_run_pipeline():
+    assert run_pipeline() == 42.64691488
+
+
+test_run_pipeline()
+#-----------------------------------------------------------------------------#
+
+# tests needed
+# - all types of phylostate tensor encoding
+# - summ stat encoding
+# - model loading
+# - model setup, events, statespace, etc.
+
