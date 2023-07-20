e42dfa72cbb71bdd7ae834ad5e0d24889710bbd5
Michael Landis <mlandis@gmail.com>
2023-07-20 07:57:06 -0500

    edit download scripts to not wipe existing projects

--- scripts/download_networks.sh
+++ scripts/download_networks.sh
@@ -4,7 +4,7 @@ MODEL=$1
 REMOTE="${llserver}"
 LOCAL_DIR="../workspace/network/${MODEL}"
 REMOTE_DIR="${REMOTE}:/home/mlandis/projects/phyddle/workspace/network"
-./clean_project.sh ${MODEL}
+#./clean_project.sh ${MODEL}
 mkdir -p ${LOCAL_DIR}
 scp "${REMOTE_DIR}/${MODEL}/sim_batchsize*_numepoch*nt*.*" ${LOCAL_DIR}
 
--- scripts/download_tensors.sh
+++ scripts/download_tensors.sh
@@ -4,7 +4,7 @@ MODEL=$1
 REMOTE="${llserver}"
 REMOTE_DIR="${REMOTE}:/home/mlandis/projects/phyddle/workspace/tensor_data"
 LOCAL_DIR="../workspace/tensor_data/${MODEL}"
-./clean_project.sh ${MODEL}
+#./clean_project.sh ${MODEL}
 mkdir -p ${LOCAL_DIR}
 scp "${REMOTE_DIR}/${MODEL}/*.hdf5" ${LOCAL_DIR}
 scp "${REMOTE_DIR}/${MODEL}/*.csv" ${LOCAL_DIR}
