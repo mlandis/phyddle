90e5912acebece9fa0f30be1dc51dafb61f32934
Michael Landis <mlandis@gmail.com>
2023-11-04 08:30:02 -0500

    verified numerical comparison for test_simulate passes/fails as intended

--- tests/test_simulate.py
+++ tests/test_simulate.py
@@ -10,16 +10,21 @@ License:   MIT
 
 import phyddle.utilities as util
 import phyddle.simulate as sim
-import os
+import numpy as np
 
 #-----------------------------------------------------------------------------#
 
 def test_sim():
 
+    # filesystem
+    base_dir = './tests/workspace/simulate'
+    test_dir = base_dir + '/test'
+    valid_dir = base_dir + '/valid'
+
     # command line arguments
     cmd_args = ['--step', 'S',
                 '--proj', 'test',
-                '--sim_dir', './tests/workspace/simulate',
+                '--sim_dir', base_dir,
                 '--sim_command', 'Rscript scripts/sim/R/sim_one.R',
                 '--end_idx', '10',
                 '--use_parallel', 'F']
@@ -33,9 +38,21 @@ def test_sim():
     # run simulator
     my_sim.run()
 
-    # verify output for sim.0.dat.nex
-    # ...
+    # confirm test and valid tree match
+    phy_test = util.read_tree(test_dir + '/sim.0.tre')
+    phy_valid = util.read_tree(valid_dir + '/sim.0.tre')
+    assert( phy_test.length() == phy_valid.length() )
+    assert( str(phy_test) == str(phy_valid) )
 
+    # confirm test and valid data match
+    dat_valid = util.convert_csv_to_array(valid_dir + '/sim.0.dat.nex', 'integer', 2)
+    dat_test = util.convert_csv_to_array(test_dir + '/sim.0.dat.nex', 'integer', 2)
+    assert( dat_test.shape == dat_valid.shape )
+    for i in range(dat_valid.shape[0]):
+        for j in range(dat_valid.shape[1]):
+            assert(dat_test.iloc[i,j] == dat_valid.iloc[i,j])
+
+    # success
     return
 
 #-----------------------------------------------------------------------------#
