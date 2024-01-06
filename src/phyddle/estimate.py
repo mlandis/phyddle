minor fixes after last major set of untested updates to network/tensor-rotation/etc.

src/phyddle/estimate.py
@@ -309,17 +309,18 @@ class Estimator:
             # point estimates & CPIs for emp. labels
             norm_emp_label_est        = torch.stack(norm_emp_label_est)[:,None,:]
             norm_emp_label_est        = norm_emp_label_est.detach().numpy()
+            #norm_emp_label_est        = torch.stack(norm_emp_label_est).detach().numpy()
             norm_emp_label_est        = np.array(norm_emp_label_est)
-            
-            norm_emp_label_est[1,:,:] = norm_emp_label_est[1,:,:] - self.cpi_adjustments[0,:]
-            norm_emp_label_est[2,:,:] = norm_emp_label_est[2,:,:] + self.cpi_adjustments[1,:]
+            norm_emp_label_est[1,:] = norm_emp_label_est[1,:] - self.cpi_adjustments[0,:]
+            norm_emp_label_est[2,:] = norm_emp_label_est[2,:] + self.cpi_adjustments[1,:]
 
             # detransform results
             emp_label_est = util.denormalize(norm_emp_label_est,
                                              self.train_labels_mean_sd,
                                              exp=True) - self.log_offset
-            
-            # save empirial label estimates
+            #print(emp_label_est)
+
+            # save empirical label estimates
             df_emp_label_est = util.make_param_VLU_mtx(emp_label_est, self.label_names)
             df_emp_label_est.to_csv(self.out_emp_label_est_fn, index=False, sep=',')
 
@@ -335,7 +336,6 @@ class Estimator:
 
         # point estimates & CPIs for test labels
         norm_test_label_est        = torch.stack(norm_test_label_est).detach().numpy()
-        # print(norm_test_label_est.shape)
         norm_test_label_est        = np.array(norm_test_label_est)
         norm_test_label_est[1,:,:] = norm_test_label_est[1,:,:] - self.cpi_adjustments[0,:]
         norm_test_label_est[2,:,:] = norm_test_label_est[2,:,:] + self.cpi_adjustments[1,:]

src/phyddle/plot.py
@@ -476,7 +476,7 @@ class Plotter:
         image = image.convert('RGB')
         
         # save as pdf
-        image.save(self.save_network_fn)
+        image.save(self.save_network_fn, dpi=(300, 300), size=(3000,2100))
         
         return
     
@@ -900,6 +900,12 @@ class Plotter:
             y_value = col_data.loc['value']
             y_lower = col_data.loc['lower']
             y_upper = col_data.loc['upper']
+
+            if plot_log:
+                y_value = np.max([1e-4, y_value])
+                y_lower = np.max([1e-4, y_lower])
+                y_upper = np.max([1e-4, y_upper])
+
             s_value = '{:.2E}'.format(y_value)
             s_lower = '{:.2E}'.format(y_lower)
             s_upper = '{:.2E}'.format(y_upper)
@@ -925,6 +931,7 @@ class Plotter:
         plt.title(title)
         plt.xticks(np.arange(num_label), label_names)
         plt.xlim( -0.5, num_label )
+        plt.ylim( )
         plt.savefig(save_fn, format='pdf', dpi=300, bbox_inches='tight')
         plt.clf()
         