864887d552dc343360acbab83ed324cea58b8482
Michael Landis <mlandis@gmail.com>
2024-02-23 12:58:40 -0600

    rm old script dir

--- scripts/util/clean_project.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-PROJECT_NAME=$1
-
-# only clean project if string not empty
-if [ -n "${PROJECT_NAME}" ]; then
-    rm -rf workspace/${PROJECT_NAME}
-fi
--- scripts/util/download_plots.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-PROJ=$1
-REMOTE=$llbh
-LOCAL_DIR="./workspace/${PROJ}/plot"
-REMOTE_DIR="${REMOTE}:/home/mlandis/projects/phyddle/workspace"
-mkdir -p ${LOCAL_DIR}
-scp "${REMOTE_DIR}/${PROJ}/plot/*.pdf" ${LOCAL_DIR}
-
