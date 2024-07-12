79614d9180bfa7068fc312080d68e5efb5afef97
Michael Landis <mlandis@gmail.com>
2024-07-11 19:41:17 -0500

    add html_baseurl

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,6 +45,7 @@ html_theme_options = {
 }
 html_static_path = ['_static']
 html_css_files = ['css/custom.css']
+html_baseurl = 'phyddle.org'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
